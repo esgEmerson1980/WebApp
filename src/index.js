f0b081358c6d4640cd4cccef7a4028875a3027b1
Emerson Gonçalves <2010785@aluno.univesp.br>
2022-04-10 19:39:27 -0300

    Alteração de correções

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Routes from './routes';
+
+
+ReactDOM.render(
+  
+  document.getElementById('root'),
+
+  < React.StrictMode >
+      <Routes />
+  </React.StrictMode>
+    
+  
+);
+
+
